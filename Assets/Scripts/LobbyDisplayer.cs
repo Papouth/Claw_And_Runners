717eb6590a3437050de2f74e2cbdd4a91d514c0b
Papouth <105810594+Papouth@users.noreply.github.com>
2023-11-08 13:45:29 +0100

    Lobby Update
    ajout du rafraichissement des serveurs ainsi que l'affichage de ceux ci

---
+++ Assets/Scripts/LobbyDisplayer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LobbyDisplayer : MonoBehaviour
+{
+    public TextMeshProUGUI numPlayerInLobby;
+    public TextMeshProUGUI maxNumPlayerInLobby;
+    public TextMeshProUGUI activeLobbyName;
+
+
+}
