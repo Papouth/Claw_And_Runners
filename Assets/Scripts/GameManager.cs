0c0439fd87b1008626d49d777e06512d6fcf3845
dupas louis <tawairpofficiel@gmail.com>
2024-04-10 00:05:47 +0200

    Timer prep
    preparation du timer pour condition de victoire defaite

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     #region Variables
     public static GameManager GM;
+    [SerializeField] private TeamSelection TS;
+    [SerializeField] private float timeRemaining;
+    [SerializeField] private TextMeshProUGUI infoTime;
     #endregion
 
     #region Built In Methods
@@ -21,6 +25,41 @@ public class GameManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(this);
+    }
+
+    private void Update()
+    {
+        // On vérifie que le décompte du start de la game est terminé
+        if (TS.readySelection)
+        {
+            Timer();
+        }
+    }
+    #endregion
+
+
+    #region Customs Methods
+    private void Timer()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            DisplayTime(timeRemaining);
+        }
+        else if (timeRemaining <= 0)
+        {
+            //Perdu();
+        }
+    }
+
+    public void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        // Envoyé ça au serveur pour le synchroniser avec les autres clients
+        //infoTime.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
     #endregion
 }
