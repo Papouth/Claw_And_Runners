4d3f7d90a7c00bcce99699603d0b99cb6f55ac2c
dupas louis <tawairpofficiel@gmail.com>
2024-06-15 16:34:27 +0200

    Opti
    opti code, on vire ce qui est pas nécessaire

--- Assets/Scripts/PlayerTeam.cs
+++ Assets/Scripts/PlayerTeam.cs
@@ -5,17 +5,8 @@ using Unity.Netcode;
 
 public class PlayerTeam : NetworkBehaviour
 {
-    public NetworkVariable<int> playerLife = new NetworkVariable<int>(readPerm: NetworkVariableReadPermission.Everyone, writePerm: NetworkVariableWritePermission.Server);
-    [SerializeField] public string team;
-
-    private NetworkParameter NP;
     private TeamSelection teamSelection;
 
-
-    private void Awake()
-    {
-        NP = NetworkManager.GetComponent<NetworkParameter>();
-    }
 
     // Quand mon joueur apparait
     public override void OnNetworkSpawn()
@@ -23,21 +14,5 @@ public class PlayerTeam : NetworkBehaviour
         teamSelection = FindObjectOfType<TeamSelection>();
 
         teamSelection.ShowHideUI();
-
-        // On affiche l'UI de team selection avec les paramètres déterminé par le nombre de joueurs via le lobbymanager
-        NP.clientCount++;
-
-        if (NP.clientCount == 1 || NP.clientCount == 3 || NP.clientCount == 5)
-        {
-            team = "red";
-        }
-        else if (NP.clientCount == 2 || NP.clientCount == 4 || NP.clientCount == 6)
-        {
-            team = "blue";
-        }
-
-        playerLife.Value = 500;
-
-        Debug.Log(gameObject.name + " à " + playerLife.Value + " HP et fait parti de l'équipe " + team);
     }
 }
