d1496b4760450116628aa91099f81411621e2f26
dupas louis <tawairpofficiel@gmail.com>
2024-06-19 03:52:18 +0200

    Fin Intégration UI Skins

--- Assets/Scripts/HomeMenuToGame.cs
+++ Assets/Scripts/HomeMenuToGame.cs
@@ -6,7 +6,25 @@ using UnityEngine.SceneManagement;
 public class HomeMenuToGame : MonoBehaviour
 {
     [SerializeField] private string gameScene;
+    [SerializeField] private GameObject panelOptions;
+    [SerializeField] private GameObject panelSkins;
+    [SerializeField] private GameObject panelHomeMenu;
 
+    // Personnages
+    [SerializeField] private GameObject[] persosPrefabs;
+
+
+    private void Start()
+    {
+        panelOptions.SetActive(false);
+        panelSkins.SetActive(false);
+        panelHomeMenu.SetActive(true);
+
+        for (int i = 0; i < persosPrefabs.Length; i++)
+        {
+            persosPrefabs[i].SetActive(false);
+        }
+    }
 
     public void GoToGameScene()
     {
@@ -16,5 +34,80 @@ public class HomeMenuToGame : MonoBehaviour
     public void LeaveGame()
     {
         Application.Quit();
+    }
+
+    public void SkinsToHomeMenu()
+    {
+        panelOptions.SetActive(false);
+        panelSkins.SetActive(false);
+        panelHomeMenu.SetActive(true);
+
+        for (int i = 0; i < persosPrefabs.Length; i++)
+        {
+            persosPrefabs[i].SetActive(false);
+        }
+    }
+
+    public void HomeMenuToSkins()
+    {
+        panelOptions.SetActive(false);
+        panelSkins.SetActive(true);
+        panelHomeMenu.SetActive(false);
+
+        ShowMaurice();
+    }
+
+    public void HomeMenuToOptions()
+    {
+        panelOptions.SetActive(true);
+        panelSkins.SetActive(false);
+        panelHomeMenu.SetActive(false);
+    }
+
+    public void OptionsToHomeMenu()
+    {
+        panelOptions.SetActive(false);
+        panelSkins.SetActive(false);
+        panelHomeMenu.SetActive(true);
+    }
+
+    public void ShowMaurice()
+    {
+        for (int i = 0; i < persosPrefabs.Length; i++)
+        {
+            persosPrefabs[i].SetActive(false);
+        }
+
+        persosPrefabs[0].SetActive(true);
+    }
+
+    public void ShowMarcel()
+    {
+        for (int i = 0; i < persosPrefabs.Length; i++)
+        {
+            persosPrefabs[i].SetActive(false);
+        }
+
+        persosPrefabs[1].SetActive(true);
+    }
+
+    public void ShowMichael()
+    {
+        for (int i = 0; i < persosPrefabs.Length; i++)
+        {
+            persosPrefabs[i].SetActive(false);
+        }
+
+        persosPrefabs[2].SetActive(true);
+    }
+
+    public void ShowMichelle()
+    {
+        for (int i = 0; i < persosPrefabs.Length; i++)
+        {
+            persosPrefabs[i].SetActive(false);
+        }
+
+        persosPrefabs[3].SetActive(true);
     }
 }
